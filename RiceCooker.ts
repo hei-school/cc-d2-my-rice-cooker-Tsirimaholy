4389ba98489da7e94db4dd2ec929cd921a44cdc2
Tsirimaholy <hei.tsirimaholy@gmail.com>
2023-12-11 16:02:00 +0300

    fix: lint

--- RiceCooker.ts
+++ RiceCooker.ts
@@ -1,155 +1,154 @@
 enum RiceCookerState {
-    IDLE = 'idle',
-    COOKING = 'cooking',
-    WARM = 'warm',
+  IDLE = 'idle',
+  COOKING = 'cooking',
+  WARM = 'warm',
 }
 
 export enum Commands {
-    COOK = 1,
-    WARM = 2,
-    CANCEL = 3,
-    ADD_WATER = 4,
-    PLUG_IN = 5,
-    UNPLUG = 6,
+  COOK = 1,
+  WARM = 2,
+  CANCEL = 3,
+  ADD_WATER = 4,
+  PLUG_IN = 5,
+  UNPLUG = 6,
 }
 
 enum ErrorCodes {
-    NO_WATER = 101,
-    NOT_PLUGGED_IN = 102,
-    BUSY = 103,
+  NO_WATER = 101,
+  NOT_PLUGGED_IN = 102,
+  BUSY = 103,
 }
 
 export class RiceCooker {
-    private state: RiceCookerState = RiceCookerState.IDLE
-    private hasWater: boolean = true
-    private isPluggedIn: boolean = true
+  private state: RiceCookerState = RiceCookerState.IDLE
+  private hasWater: boolean = true
+  private isPluggedIn: boolean = true
 
-    cookRice() {
-        if (!this.isPluggedIn) {
-            console.error(`Error: ${ErrorCodes.NOT_PLUGGED_IN} - Rice cooker is not plugged in.`)
-            return
-        }
+  cookRice (): void {
+    if (!this.isPluggedIn) {
+      console.error(
+        `Error: ${ErrorCodes.NOT_PLUGGED_IN} - Rice cooker is not plugged in.`
+      )
+    } else if (!this.hasWater) {
+      console.error(
+        `Error: ${ErrorCodes.NO_WATER} - No water detected. Please add water and try again.`
+      )
+    } else if (this.state !== RiceCookerState.IDLE) {
+      console.log('Rice cooker is already busy.')
+    } else {
+      this.state = RiceCookerState.COOKING
+      console.log('Cooking rice...')
 
-        if (!this.hasWater) {
-            console.error(`Error: ${ErrorCodes.NO_WATER} - No water detected. Please add water and try again.`)
-            return
-        }
-
-        if (this.state !== RiceCookerState.IDLE) {
-            console.log('Rice cooker is already busy.')
-            return
-        }
-
-        this.state = RiceCookerState.COOKING
-        console.log('Cooking rice...')
-
-        this.cookRiceAsync().then(value => {
-            console.log('Rice is cooked!')
-        }).catch((error) => {
-            console.error(`Error: ${error}`)
-        }).finally(() => {
-                this.state = RiceCookerState.IDLE
-            }
-        )
-    }
-
-    private cookRiceAsync(): Promise<void> {
-        return new Promise(resolve => {
-            setTimeout(() => {
-                resolve()
-            }, 3000)
+      this.cookRiceAsync()
+        .then((value) => {
+          console.log('Rice is cooked!')
+        })
+        .catch((error) => {
+          console.error(`Error: ${error as string}`)
+        })
+        .finally(() => {
+          this.state = RiceCookerState.IDLE
         })
     }
+  }
 
-    warmRice() {
-        if (!this.isPluggedIn) {
-            console.error(`Error: ${ErrorCodes.NOT_PLUGGED_IN} - Rice cooker is not plugged in.`)
-            return
-        }
+  private async cookRiceAsync (): Promise<void> {
+    return await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve()
+      }, 3000)
+    })
+  }
 
-        if (!this.hasWater) {
-            console.error(`Error: ${ErrorCodes.NO_WATER} - No water detected. Please add water and try again.`)
-            return
-        }
+  warmRice (): void {
+    if (!this.isPluggedIn) {
+      console.error(
+        `Error: ${ErrorCodes.NOT_PLUGGED_IN} - Rice cooker is not plugged in.`
+      )
+    } else if (!this.hasWater) {
+      console.error(
+        `Error: ${ErrorCodes.NO_WATER} - No water detected. Please add water and try again.`
+      )
+    } else if (
+      this.state !== RiceCookerState.IDLE &&
+      this.state !== RiceCookerState.WARM
+    ) {
+      console.log('Rice cooker is not ready for warming.')
+    } else {
+      this.state = RiceCookerState.WARM
+      console.log('Warming rice...')
 
-        if (this.state !== RiceCookerState.IDLE && this.state !== RiceCookerState.WARM) {
-            console.log('Rice cooker is not ready for warming.')
-            return
-        }
-
-        this.state = RiceCookerState.WARM
-        console.log('Warming rice...')
-
-        // Simulate warming time
-        setTimeout(() => {
-            console.log('Rice is warm and ready to serve!')
-            this.state = RiceCookerState.IDLE
-        }, 1000)
-    }
-
-    cancel() {
-        if (this.state === RiceCookerState.IDLE) {
-            console.log('Nothing to cancel.')
-            return
-        }
-
-        console.log('Canceling operation...')
+      // Simulate warming time
+      setTimeout(() => {
+        console.log('Rice is warm and ready to serve!')
         this.state = RiceCookerState.IDLE
-        console.log('Operation canceled.')
+      }, 1000)
     }
+  }
 
-    addWater() {
-        this.hasWater = true
-        console.log('Water added successfully.')
+  cancel (): void {
+    if (this.state === RiceCookerState.IDLE) {
+      console.log('Nothing to cancel.')
+    } else {
+      console.log('Canceling operation...')
+      this.state = RiceCookerState.IDLE
+      console.log('Operation canceled.')
     }
+  }
 
-    plugIn() {
-        this.isPluggedIn = true
-        console.log('Rice cooker plugged in.')
+  addWater (): void {
+    this.hasWater = true
+    console.log('Water added successfully.')
+  }
+
+  plugIn (): void {
+    this.isPluggedIn = true
+    console.log('Rice cooker plugged in.')
+  }
+
+  unplug (): void {
+    if (this.state !== RiceCookerState.IDLE) {
+      console.error(
+        `Error: ${ErrorCodes.BUSY} - Rice cooker is busy. Please wait until it finishes before unplugging.`
+      )
+    } else {
+      this.isPluggedIn = false
+      console.log('Rice cooker unplugged.')
     }
+  }
 
-    unplug() {
-        if (this.state !== RiceCookerState.IDLE) {
-            console.error(`Error: ${ErrorCodes.BUSY} - Rice cooker is busy. Please wait until it finishes before unplugging.`)
-            return
-        }
+  printMenu (): void {
+    console.log('\nMenu:')
+    console.log('1. Cook Rice')
+    console.log('2. Warm Rice')
+    console.log('3. Cancel')
+    console.log('4. Add Water')
+    console.log('5. Plug In')
+    console.log('6. Unplug')
+  }
 
-        this.isPluggedIn = false
-        console.log('Rice cooker unplugged.')
+  handleCommand (command: Commands): void {
+    switch (command) {
+      case Commands.COOK:
+        this.cookRice()
+        break
+      case Commands.WARM:
+        this.warmRice()
+        break
+      case Commands.CANCEL:
+        this.cancel()
+        break
+      case Commands.ADD_WATER:
+        this.addWater()
+        break
+      case Commands.PLUG_IN:
+        this.plugIn()
+        break
+      case Commands.UNPLUG:
+        this.unplug()
+        break
+      default:
+        console.log('Invalid command. Please enter a valid option.')
     }
-
-    printMenu(): void {
-        console.log('\nMenu:')
-        console.log('1. Cook Rice')
-        console.log('2. Warm Rice')
-        console.log('3. Cancel')
-        console.log('4. Add Water')
-        console.log('5. Plug In')
-        console.log('6. Unplug')
-    }
-
-    handleCommand(command: Commands) {
-        switch (command) {
-            case Commands.COOK:
-                this.cookRice()
-                break
-            case Commands.WARM:
-                this.warmRice()
-                break
-            case Commands.CANCEL:
-                this.cancel()
-                break
-            case Commands.ADD_WATER:
-                this.addWater()
-                break
-            case Commands.PLUG_IN:
-                this.plugIn()
-                break
-            case Commands.UNPLUG:
-                this.unplug()
-                break
-            default:
-                console.log('Invalid command. Please enter a valid option.')
-        }
-    }
+  }
 }
--- main.ts
+++ main.ts
@@ -1,28 +1,28 @@
-import {Commands, RiceCooker} from "./RiceCooker";
-import readline from "readline";
+import { Commands, RiceCooker } from './RiceCooker'
+import readline from 'readline'
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+  input: process.stdin,
+  output: process.stdout
+})
 
-const cooker = new RiceCooker();
+const cooker = new RiceCooker()
 
-function processUserInput() {
-    cooker.printMenu();
+function processUserInput (): void {
+  cooker.printMenu()
 
-    rl.question('Enter your command: ', (answer) => {
-        const userChoice = parseInt(answer.trim(), 10);
+  rl.question('Enter your command: ', (answer) => {
+    const userChoice = parseInt(answer.trim(), 10)
 
-        if (isNaN(userChoice)) {
-            console.log('Invalid input. Please enter a number.');
-        } else {
-            const command = userChoice as Commands;
-            cooker.handleCommand(command);
-        }
+    if (isNaN(userChoice)) {
+      console.log('Invalid input. Please enter a number.')
+    } else {
+      const command = userChoice as Commands
+      cooker.handleCommand(command)
+    }
 
-        processUserInput();
-    });
+    processUserInput()
+  })
 }
 
-processUserInput();
+processUserInput()
